88cb1df9faace82e85671eab763a6c84a3e1414f
Tetsuro Kurita <tkurita@mac.com>
2015-08-31 13:11:44 +0000

    cp CocoaLib/branches/ARC/StringExtra.h StringExtra/trunk
    git-svn-id: file:///Users/tkurita/Dev/svnrepos/StringExtra/trunk@4694 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ StringExtra.h
@@ -0,0 +1,29 @@
+#import <Cocoa/Cocoa.h>
+
+@interface NSMutableString (StringExtra)
+
+- (unsigned int)replacePrefixOfString:(NSString *)target withString:(NSString *)replacement options:(unsigned int)opts;
+- (unsigned int)replaceSuffixOfString:(NSString *)target withString:(NSString *)replacement options:(unsigned int)opts;
+
+@end
+
+@interface NSString (StringExtra) 
+
+- (NSMutableString *)normalizedString:(CFStringNormalizationForm) theForm;
+- (BOOL)isEqualToNormalizedString:(NSString *)targetString;
+- (BOOL)contain:(NSString *)containedText; 
+- (BOOL)contain:(NSString *)target options:(unsigned int)opts;
+- (BOOL)hasPrefix:(NSString *)text options:(unsigned int)opts;
+- (BOOL)hasSuffix:(NSString *)text options:(unsigned int)opts;
++ (NSString *)stringWithData:(NSData *)aData encodingCandidates:(NSArray *)encodings;
+- (NSMutableArray *)splitWithCharacterSet:(NSCharacterSet *)delimiters;
+- (NSMutableArray *)paragraphs;
+// get file type from POSX path
+// mainly called from AppleScript
+- (NSString *)hfsFileType;
+
+- (NSString *)stringByAddingPercentEscapesUsingEncoding:(NSStringEncoding)encoding 
+											   leavings:(NSString *)charactersToLeaveUnescaped
+											additionals:(NSString *)legalURLCharactersToBeEscaped;
++ (id)stringWithLocalizedFormat:(NSString *)format, ...;
+@end
